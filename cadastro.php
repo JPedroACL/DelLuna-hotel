4a974b3c3f80a11d4f964f388e79751ec88f261b
JPedroACL <105823539+JPedroACL@users.noreply.github.com>
2022-06-27 13:00:53 -0300

    cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,7 +1,7 @@
 <?php
-
+//include 'salvar.php';
 ?>
-  
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,14 +10,13 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   <link rel="stylesheet" href="teste.css">
   <link rel="stylesheet" href="bootstrap.min.css">
   <link rel="stylesheet" href="js.js">
-  
 
-  
+
+
 
   <title>Document</title>
 </head>
@@ -25,52 +24,50 @@
 <body>
 
   <div class="container">
-   <center>
+    <center>
       <div class="col-sm-3">
 
         <h2 class="titulo">Cadastre o Cliente</h2>
 
         <br>
 
-      <form action="menu.php" method="POST" >
+        <form action="salvar.php" method="POST">
 
 
-         <label> Nome:</label> <br>
-          <input type="text" class="form-control" name="nome" placeholder="Nome do Cliente" required > <i
-            class="bi bi-person-fill"></i><br>
-            <label> Email:</label <br>
-          <input type="Email" class="form-control" name="email" placeholder="Digite Email do Cliente" required ><br>
+          <label> Nome:</label> <br>
+          <input type="text" class="form-control" name="nome" placeholder="Nome do Cliente" required> <i class="bi bi-person-fill"></i><br>
+          <label> Email:</label <br>
+          <input type="Email" class="form-control" name="email" placeholder="Digite Email do Cliente" required><br>
+
+
+          Telefone: <input type="tel" class="form-control" placeholder="(00) 0 0000-0000" maxlength="11" id="phone" oninput="mascaratel('tele')" inputmode="numeric" name="telefone" required>
+          <br>
+
+          Tem crianças ?: <input type="number" min="0" max="5" name="criancas"><br><br>
+
+          Quantidade de quartos: <input type="number" min="1" max="4" name="quartos"><br><br>
+
 
           <label> Check-in:</label <br>
           <input type="date" class="form-control" placeholder="" name="Data"><br>
 
-          Telefone: <input type="tel" class="form-control"
-          placeholder="(00) 0 0000-0000" maxlength="11" id="phone"
-          oninput="mascaratel('tele')" inputmode="numeric" name="telefone" required> 
-          <br>
-
-          Tem crianças ?: <input type="number" min="0" max="5"   name="criancas"><br><br>
-              
-
-          
-
-          Quantidade de quartos: <input type="number" min="1" max="4" name="quartos"><br><br>
-
           Funcionario: <input type="text" class="form-control" placeholder="Nome do funcionário" name="func"><br>
 
           preferências :<input type="text" class="form-control" placeholder="Digite as preferências" name="pref"><br>
-            <a href="menu.php"><button class="btn btn-success" onclick="validar()" name="submit">Cadastrar-se</button>
-              </a>
-          </center>
-
-</form>
+          <a href="menu.php"><button class="btn btn-success" onclick="validar()" name="submit">Cadastrar-se</button>
+          </a>
+    
+      
+    </form>
+    </div>
+    </center> 
   </div>
-     </div>
   
+
 
   <script src="js.js"></script>
 
-      
+
 
 </body>
 
