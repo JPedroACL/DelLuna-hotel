f00d1a038cc521bdbba1c5a4a0a9016ffee84804
JPedroACL <105823539+JPedroACL@users.noreply.github.com>
2022-06-27 02:40:39 -0300

    Create conexao.php

---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php
+ $hostname = "localhost";
+ $usuario = "root";
+ $senha = "";
+ $bancodedados = "delluna";
+
+
+$mysqli = mysqli_connect($hostname, $usuario, $senha, $bancodedados);
+if(!$mysqli){
+    echo "Falha ao conectar:";
+}
+else{
+    echo "Conectado";
+}
